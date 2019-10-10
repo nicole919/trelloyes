a0b0de52201c9c57dd48f3d36d73fae2613118df
Nicole Fafard <nicole.fafard@gmail.com>
2019-10-10 15:57:54 -0700

    added functionality to buttons

--- src/components/List.js
+++ src/components/List.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import Card from './Card'
-import './List.css';
+import React from "react";
+import Card from "./Card";
+import "./List.css";
 
 function List(props) {
-    return (
-        <section className='List'>
-            <header className='List-header'>
-                <h2>{props.header}</h2>
-            </header>
-            <div className='List-cards'>
-                {props.cards.map((card) =>
-                    <Card
-                        key={card.id}
-                        title={card.title}
-                        content={card.content}
-                    />
-                )}
-                <button
-                    type='button'
-                    className='List-add-button'
-                >
-                    + Add Random Card
-            </button>
-            </div>
-        </section>
-    )
+  return (
+    <section className="List">
+      <header className="List-header">
+        <h2>{props.header}</h2>
+      </header>
+      <div className="List-cards">
+        {props.cards.map(card => (
+          <Card
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            content={card.content}
+            onClickDelete={props.onClickDelete}
+          />
+        ))}
+        <button
+          type="button"
+          className="List-add-button"
+          onClick={() => props.onClickAdd(props.id)}
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  );
 }
 
+List.defaultProps = {
+  onClickAdd: () => {}
+};
 export default List;
